29c002e0cab14db522fc00b5ff8172943aa97cb2
aPaulTaylor <paul.a.taylor@gmail.com>
2022-05-08 10:23:05 +0100

    data and parsers

--- charts-scraper/charts-data-parser.py
+++ charts-scraper/charts-data-parser.py
@@ -1,23 +1,34 @@
 import os
 import pandas as pd
 import re
+import datetime
 
 #all_charts=pd.DataFrame()
 
 #for fn in os.listdir('charts-data'):
-#    top_100 = pd.read_csv('charts-data/'+fn)
-#    top_100['week']=fn.split('.')[0][7:]
-#    all_charts = all_charts.append(top_100)
+#    if 'top100' in fn:
+#        top_100 = pd.read_csv('charts-data/'+fn)
+#        top_100['week']=fn.split('.')[0][7:]
+#        all_charts = all_charts.append(top_100)
 
-#all_charts.to_csv('charts-data/all-singles-charts.csv',index=False)
+all_charts.to_csv('charts-data/all-singles-charts.csv',index=False)
 
 all_charts=pd.read_csv('charts-data/all-singles-charts.csv')
 
 def sanitize_title(title):
     title = re.sub('\(.*\)','',title)
-    title = re.sub('[^A-Za-z0_9\s]','',title)
+    title = re.sub('{.*}', '', title)
+    title = re.sub('[^A-Za-z0-9\s]','',title)
     title = title.strip()
     return title
 
 all_charts['sanitized_title'] = all_charts.apply(lambda row: sanitize_title(row['titles']), axis=1)
-print(all_charts.head(10))
+all_charts['title_signature'] = all_charts.apply(lambda row: tuple(map(len,row['sanitized_title'].split(' '))), axis=1)
+all_charts['week_commencing'] = all_charts.apply(lambda row: datetime.datetime.strptime(row['week'], "%d %B %Y") , axis=1)
+
+all_charts['sanitized_title'][(all_charts['title_signature']==(5,5)) & (all_charts['positions']<41)].nunique()
+
+top40_songs=all_charts[(all_charts['positions']<41)].groupby(['artists','titles','sanitized_title','title_signature']).agg({'positions':min,'week_commencing':min})
+top40_songs.reset_index(inplace=True)
+top40_songs.columns=['artist','title','sanitized_title','title_signature','top_position','first_week']
+top40_songs.to_csv('charts-data/top_40_songs.csv',index=False)
--- charts-scraper/charts-scraper.py
+++ charts-scraper/charts-scraper.py
@@ -9,21 +9,27 @@ def get_chart(url):
     chart_date = soup.find_all('p', class_='article-date')[0].text.split('-')[0].strip()
     artists_txt = [a.text.strip() for a in  soup.find_all('div', class_='artist') ]
     titles_txt = [a.text.strip() for a in  soup.find_all('div', class_='title') ]
+    positions = [int(a.text.strip()) for a in  soup.find_all('span', class_='position') ]
     print(titles_txt)
+    print(positions)
     top_100 = pd.DataFrame({
-        'positions':range(1,101),
+        'positions':positions,
         'artists':artists_txt,
         'titles':titles_txt
     })
     prevlink = 'https://www.officialcharts.com'+soup.find('a', text ='prev')['href']
     return top_100, chart_date, prevlink
 
-url = 'https://www.officialcharts.com/charts/singles-chart/20220325/7501/'
+url = 'https://www.officialcharts.com/charts/singles-chart/19521218/7501/'
 chart_date=''
 
-while '1952' not in chart_date:
+while '1951' not in chart_date:
     top_100, chart_date, prevlink = get_chart(url)
     print(prevlink)
     top_100.to_csv(f'charts-data/top100 {chart_date}.csv', index=False)
     url=prevlink
 
+url = 'https://www.officialcharts.com/charts/singles-chart/19810301/7501/'
+top_100, chart_date, prevlink = get_chart(url)
+print(prevlink)
+top_100.to_csv(f'charts-data/top100 {chart_date}.csv', index=False)
